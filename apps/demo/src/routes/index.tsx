ef1713b0484c1ce1b173b64ffbf1ed6a5dc6da8f
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-07-28 19:06:29 -0400

    ongoing work

--- apps/demo/src/routes/index.tsx
+++ apps/demo/src/routes/index.tsx
@@ -4,9 +4,9 @@ export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
 	return (
-		<div className='p-8'>
-			<h1 className='text-4xl font-bold'>Welcome to TanStack Start</h1>
-			<p className='mt-4 text-lg'>
+		<div>
+			<h1>Welcome to TanStack Start</h1>
+			<p>
 				Edit <code>src/routes/index.tsx</code> to get started.
 			</p>
 		</div>
