b081f863820dac2b3e77562d9f04c7f1c5586116
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-07-28 19:15:03 -0400

    ongoing work

--- apps/demo/src/routes/__root.tsx
+++ apps/demo/src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { TanStackDevtools } from '@tanstack/react-devtools'
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 
-import { getLocale } from '#/paraglide/runtime'
+import { getLocale } from '@/paraglide/runtime'
 
 import appCss from '../styles.css?url'
 
